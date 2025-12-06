b218aa94eae14f1fe0e5d880dfb18f549885bb6b
kayoooma <kamasultanov8@gmail.com>
2025-12-06 07:42:27 +0500

    Cleanup html

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // IMPORTANT: Change 'quantum-coin' to your actual GitHub repository name
-  // e.g. base: '/my-crypto-site/'
-  base: '/quantum-coin/', 
+  // Sets the base path for GitHub Pages deployment
+  base: '/Quantum_coin/', 
 })
