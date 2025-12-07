0f3872f0f59ead7a2b313b780ead29c8cbb02e8e
kayoooma <kamasultanov8@gmail.com>
2025-12-07 10:31:59 +0500

    trying to fix the black screen

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // Base set to './' makes assets load relatively, solving the black screen issue
-  base: './', 
+  base: '/Quantum_coin/',
 })
